f2c9ee18d9848c83b2e3557d5b97f773c94fce16
Fordalex <Fordalex@users.noreply.github.com>
2025-02-20 12:04:15 +0000

    deploy: 5691e6955c1b130a959b3b0ea81f3f1f466a9587

--- assets/VSpacer-Bq5flNHv.js
+++
@@ -1,1 +0,0 @@
-import{aR as a}from"./index-DtQiKd8H.js";const r=a("v-spacer","div","VSpacer");export{r as V};
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/check-mate/sw.js', { scope: '/check-mate/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/momentum/sw.js', { scope: '/momentum/' })})}
